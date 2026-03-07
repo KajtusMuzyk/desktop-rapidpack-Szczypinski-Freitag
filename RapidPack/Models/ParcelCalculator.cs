30146492b68342955ec3878849090a6b5a32ab64
Kajetan Szczypiński <kajetan.szczypinski@cyfroweszkoly.pl>
2026-03-07 22:08:50 +0100

    create parcelcalculator.cs and validate weight

---
+++ RapidPack/Models/ParcelCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RapidPack.Models
+{
+    public class ParcelCalculator
+    {
+        private const double MaxWeight = 30.0;
+
+        public void ValidateWeight(double weight)
+        {
+            if (weight > MaxWeight)
+            {
+                throw new ArgumentException($"Paczka jest za ciężka ({weight} kg). Maksimum to {MaxWeight} kg!");
+            }
+        }
+    }
+}
